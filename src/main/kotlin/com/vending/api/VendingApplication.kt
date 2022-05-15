a9e16b0fc01a1b0d176377035ad2f1042a775945
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-16 00:43:20 +0100

    initial commit

---
+++ src/main/kotlin/com/vending/api/VendingApplication.kt
@@ -0,0 +1,12 @@
+package com.vending.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+class VendingApplication
+
+fun main(args: Array<String>) {
+    runApplication<VendingApplication>(*args)
+}
---
+++ src/test/kotlin/com/vending/api/VendingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vending.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VendingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
