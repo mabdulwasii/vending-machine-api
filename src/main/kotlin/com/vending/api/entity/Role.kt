0d0bcddb4d8d8437587832e725175149aeb8db49
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-16 00:52:21 +0100

    refactor role entity

--- src/main/kotlin/com/vending/api/entity/Role.kt
+++ src/main/kotlin/com/vending/api/entity/Role.kt
@@ -3,46 +3,23 @@ package com.vending.api.entity
 import com.vending.api.entity.enumeration.RoleType
 import org.hibernate.annotations.Cache
 import org.hibernate.annotations.CacheConcurrencyStrategy
-import java.io.Serializable
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
-@Table(name = "authority")
+@Table(name = "role")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-class Role : Serializable {
-    constructor()
-    constructor(id: Int, name: RoleType?) {
-        this.id = id
-        this.name = name
-    }
-
+class Role(
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private var id = 0
+    @GeneratedValue
+    var id: Long? = null,
 
     @Enumerated(EnumType.STRING)
     @Column(name = "name", nullable = false, length = 50)
-    private var name: RoleType? = null
-    fun getName(): RoleType? {
-        return name
-    }
-
-    // prettier-ignore
-    override fun toString(): String {
-        return "Authority{" +
-                "name='" + name + '\'' +
-                "}"
-    }
-
-    companion object {
-        private const val serialVersionUID = 1L
-    }
-}
+    var name: RoleType
+)
