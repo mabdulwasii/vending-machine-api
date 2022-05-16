586fc4ad121fdae84d574ad056bb27f2b7a44b6c
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-16 02:27:17 +0100

    create password validation annotation

---
+++ src/main/kotlin/com/vending/api/utils/PasswordValidatorConstraint.kt
@@ -0,0 +1,37 @@
+package com.vending.api.utils
+
+import org.passay.CharacterRule
+import org.passay.EnglishCharacterData
+import org.passay.LengthRule
+import org.passay.PasswordData
+import org.passay.PasswordValidator
+import org.passay.WhitespaceRule
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+
+class PasswordValidatorConstraint : ConstraintValidator<ValidPassword?, String?> {
+    override fun initialize(constraintAnnotation: ValidPassword?) {}
+    override fun isValid(password: String?, context: ConstraintValidatorContext): Boolean {
+        val validator = PasswordValidator(
+            listOf( // at least 8 characters
+                LengthRule(8, 255),  // at least one upper-case character
+                CharacterRule(EnglishCharacterData.UpperCase, 1),  // at least one lower-case character
+                CharacterRule(EnglishCharacterData.LowerCase, 1),  // at least one digit character
+                CharacterRule(EnglishCharacterData.Digit, 1),  // at least one symbol (special character)
+                CharacterRule(EnglishCharacterData.Special, 1),  // no whitespace
+                WhitespaceRule() // no common passwords
+                //            dictionaryRule
+            )
+        )
+        val result = validator.validate(PasswordData(password))
+        if (result.isValid) {
+            return true
+        }
+        val messages = validator.getMessages(result)
+        val messageTemplate = java.lang.String.join(",", messages)
+        context.buildConstraintViolationWithTemplate(messageTemplate)
+            .addConstraintViolation()
+            .disableDefaultConstraintViolation()
+        return false
+    }
+}
---
+++ src/main/kotlin/com/vending/api/utils/ValidPassword.kt
@@ -0,0 +1,15 @@
+package com.vending.api.utils
+
+import javax.validation.Constraint
+import javax.validation.Payload
+import kotlin.reflect.KClass
+
+@MustBeDocumented
+@Constraint(validatedBy = [PasswordValidatorConstraint::class])
+@Target(AnnotationTarget.FIELD, AnnotationTarget.ANNOTATION_CLASS)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class ValidPassword(
+    val message: String = "Password must be 8 or more characters in length.Password must contain 1 or more uppercase characters.Password must contain 1 or more lowercase characters",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
