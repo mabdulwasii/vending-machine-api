052bd3638a978fadbb892d5d799439e2432889e8
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-16 22:38:50 +0100

    some package refactoring

--- src/main/kotlin/com/vending/api/utils/password/PasswordValidatorConstraint.kt
+++ src/main/kotlin/com/vending/api/utils/password/PasswordValidatorConstraint.kt
@@ -1,4 +1,4 @@
-package com.vending.api.utils
+package com.vending.api.utils.password
 
 import org.passay.CharacterRule
 import org.passay.EnglishCharacterData
--- src/main/kotlin/com/vending/api/utils/password/ValidPassword.kt
+++ src/main/kotlin/com/vending/api/utils/password/ValidPassword.kt
@@ -1,4 +1,4 @@
-package com.vending.api.utils
+package com.vending.api.utils.password
 
 import javax.validation.Constraint
 import javax.validation.Payload
