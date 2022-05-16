b1a6413b4f76a845a6677434133f77e879fd80c2
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-16 22:40:59 +0100

    update role and user repositories

--- src/main/kotlin/com/vending/api/repository/RoleRepository.kt
+++ src/main/kotlin/com/vending/api/repository/RoleRepository.kt
@@ -1,8 +1,9 @@
 package com.vending.api.repository
 
 import com.vending.api.entity.Role
-import com.vending.api.entity.User
+import com.vending.api.entity.enumeration.RoleType
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface RoleRepository : JpaRepository<Role, Long> {
+    fun findByName(name: RoleType): Role?
 }
--- src/main/kotlin/com/vending/api/repository/UserRepository.kt
+++ src/main/kotlin/com/vending/api/repository/UserRepository.kt
@@ -4,4 +4,5 @@ import com.vending.api.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long> {
+    fun findOneWithAuthoritiesByUsernameIgnoreCase(username: String): User?
 }
