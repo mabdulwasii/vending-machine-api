875ca3ae27b0666cdb78a72415f3091d0b18ca72
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-17 23:35:18 +0100

    code cleanup

--- src/main/kotlin/com/vending/api/dto/ApiResponse.kt
+++ src/main/kotlin/com/vending/api/dto/ApiResponse.kt
@@ -1,10 +1,12 @@
 package com.vending.api.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.http.HttpStatus
 
 data class ApiResponse(
     val error: Boolean,
     val message: String,
     val data: Any,
+    @JsonIgnore
     val status: HttpStatus
 )
--- src/main/resources/db/migration/V1__Initial.sql
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -10,29 +10,21 @@ create TABLE IF NOT EXISTS user
     deposit  INT                  NOT NULL COMMENT 'User deposit',
     role VARCHAR(50),
     PRIMARY KEY (id)
-    ) ;
+) ;
 
 CREATE TABLE IF NOT EXISTS role
 (
     id   INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
-    );
+);
 
 CREATE TABLE IF NOT EXISTS user_role
 (
     id           INT PRIMARY KEY AUTO_INCREMENT COMMENT 'user role id',
     user_id      BIGINT COMMENT 'User id',
     role_id VARCHAR(50) COMMENT 'Role id'
-    );
-
-INSERT INTO user (id, username, password, deposit)
-VALUES (1, 'admin@example.com', '$2a$04$Ot6tX0QK8xzo/xW5A/J3F.QZDS7eio095dN5IoQjWJDOySs42f1S.', 0),
-       (2, 'user@example.com', '$2a$04$Ot6tX0QK8xzo/xW5A/J3F.QZDS7eio095dN5IoQjWJDOySs42f1S.', 0);
+);
 
 INSERT INTO role (id, name)
-VALUES (1, 'ROLE_ADMIN'),
-       (2, 'ROLE_USER');
-
-INSERT INTO user_role (user_id, role_id)
-VALUES (1, 1),
-       (2, 2);
+VALUES (1, 'ROLE_BUYER'),
+       (2, 'ROLE_SELLER');
