d552b82759ebaabcbf4e65108cdce1835bb1d1be
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-17 02:16:33 +0100

    create product entity

---
+++ src/main/kotlin/com/vending/api/entity/Product.kt
@@ -0,0 +1,37 @@
+package com.vending.api.entity
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+@Entity
+@Table(name = "product")
+class Product(
+    @Id
+    @GeneratedValue
+    var id: Long? = null,
+
+    @NotNull
+    @Column(name = "amount_available")
+    var amountAvailable: Int = 0,
+
+    @NotNull
+    var cost: Int = 0,
+
+    @NotNull
+    @Size(min = 1, max = 256)
+    @Column(name = "product_name", length = 256, nullable = false)
+    var productName: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id")
+    var seller: User ,
+
+)
