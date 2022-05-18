e552764881daccbbd8cb6d0600d5e1d1c191ea6e
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-18 16:03:21 +0100

    Add method for failure api response

--- src/main/kotlin/com/vending/api/utils/ApiResponseUtils.kt
+++ src/main/kotlin/com/vending/api/utils/ApiResponseUtils.kt
@@ -5,18 +5,32 @@ import org.springframework.http.HttpStatus
 
 class ApiResponseUtils {
     companion object {
-        fun buildSuccessfulApiResponse(data: Any?, message: String, status: HttpStatus) = ApiResponse(
+        fun buildSuccessApiResponse(data: Any?, message: String, status: HttpStatus) = ApiResponse(
             error = false,
             message = message,
             data = data,
             status = status
         )
 
-        fun buildSuccessfulApiResponse(data: Any?, message: String) = ApiResponse(
+        fun buildSuccessApiResponse(data: Any?, message: String) = ApiResponse(
             error = false,
             message = message,
             data = data,
             status = HttpStatus.OK
+        )
+
+        fun buildFailedApiResponse(data: Any?, message: String, status: HttpStatus) = ApiResponse(
+            error = true,
+            message = message,
+            data = data,
+            status = status
+        )
+
+        fun buildFailedApiResponse(data: Any?, message: String) = ApiResponse(
+            error = true,
+            message = message,
+            data = data,
+            status = HttpStatus.BAD_REQUEST
         )
     }
 }
