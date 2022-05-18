27881e01dac11b2c8da7ca4688bef90ce4f5f37d
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-05-18 16:02:17 +0100

    refactor validCent to validcentAmount

--- src/main/kotlin/com/vending/api/utils/validation/CentValidatorConstraint.kt
+++ src/main/kotlin/com/vending/api/utils/validation/CentValidatorConstraint.kt
@@ -1,10 +1,10 @@
-package com.vending.api.utils.validate
+package com.vending.api.utils.validation
 
 import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 
-class CentValidatorConstraint : ConstraintValidator<ValidCent, Int> {
-    override fun initialize(constraintAnnotation: ValidCent) {}
+class CentValidatorConstraint : ConstraintValidator<ValidCentAmount, Int> {
+    override fun initialize(constraintAnnotation: ValidCentAmount) {}
     override fun isValid(value: Int, context: ConstraintValidatorContext): Boolean {
         val validCent = listOf(5, 10, 20, 50, 100)
 
